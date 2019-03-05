c3347ad3e23fb08e6a94d31206e1566818659950
MacStudent <macstudent@126-imac14.local>
2019-03-05 17:55:04 -0500

    day 2 buttons and files added of calculator

--- day2ios/ViewController.swift
+++ day2ios/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var a: Int = 0
+    var b: Int = 0
+    var c: Int = 0
+    @IBOutlet weak var txtno1: UITextField!
+    @IBOutlet weak var txtno2: UITextField!
+    @IBOutlet weak var lblresult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func btnADD(_ sender: UIButton) {
+        a = Int(txtno1.text!)!
+        b = Int(txtno2.text!)!
+        c = a + b
+        lblresult.text = String(c)
+    }
+ 
+    @IBAction func btnSub(_ sender: UIButton) {
+        a = Int(txtno1.text!)!
+        b = Int(txtno2.text!)!
+        c = a - b
+        lblresult.text = String(c)
+    }
+    
+    @IBAction func btnSumbit(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    }
+    
 
-}
 
