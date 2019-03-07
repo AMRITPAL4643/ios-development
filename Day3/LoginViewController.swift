07a50925459ab28133be433673ed0b17f70d46c7
MacStudent <macstudent@126-imac14.local>
2019-03-06 20:37:58 -0500

    correct navigation added

--- Day3/LoginViewController.swift
+++ Day3/LoginViewController.swift
@@ -31,15 +31,13 @@ class LoginViewController: UIViewController {
         {
             if LoginPassword.text == "amrit"
             {
-//                var alert = UIAlertController(title: "alert", message: "login sucessfully", preferredStyle: .alert)
-//                var action3 = UIAlertAction(title: "ok", style: .default , handler: nil)
-//                alert.addAction(action3)
-//                self.present(alert, animated: true)
-                
+             //var alert = UIAlertController(title: "alert", message: "login sucessfully", preferredStyle: .alert)
+             //   let action3 = UIAlertAction(title: "ok", style: .default , handler: nil);               alert.addAction(action3)
+             //  self.present(alert, animated: true)
                 let sb = UIStoryboard(name: "Main", bundle: nil)
-                let homeVC = sb.instantiateViewController(withIdentifier: "Homevc") as! HomeViewController
-                self.navigationController?.pushViewController(homeVC, animated: true)
-                
+                let homevc = sb.instantiateViewController(withIdentifier: "HomeVC") as! HomeViewController
+                self.navigationController?.pushViewController(homevc, animated: true)
+               
                 
                 
             }else
