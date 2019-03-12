46a068acc954909bdc75fa1abfb0675d1d53820f
MacStudent <macstudent@126-imac14.local>
2019-03-12 19:56:05 -0400

    plist class code added

--- day8.plistEmployList/ViewController.swift
+++ day8.plistEmployList/ViewController.swift
@@ -12,9 +12,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.readInformationPlist()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+   
+    
+    func readInformationPlist() {
+        if let bundlePath = Bundle.main.path(forResource: "Employ", ofType: "plist") {
+            let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+            
+            print(dictionary!.description)
+            let countryList = dictionary!["countries"] as! NSArray
+           
+            print(countryList[0])
+            for c in countryList
+            {
+                print(c)
+            }
+            
+            print("emplyees list")
+            let employeesList = dictionary!["employees"] as! NSArray
+            
+            print(employeesList[0])
+            for e in employeesList
+            {
+                print(e)
+        }
+             let EmployeesList = dictionary!["employees"] as! NSArray
+            for a in EmployeesList
+            {
+                let emp = a as! NSDictionary
+                print("employee Name : \(emp["ename"]!)")
+                print("employee ID : \(emp["eid"]!)")
+                print("employee salary : \(emp["Salary"]!)")
+                
+            }
+}
+        
 
 }
-
+}
